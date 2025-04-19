d8d32bcda3e3d7119ed203bfefb88bbc4a31d944
Abdul Kadir Olia <abdulkadirolia@abduls-mbp.mynetworksettings.com>
2025-04-19 10:44:03 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -36,13 +38,47 @@ func handleRequest(conn net.Conn) (err error) {
 	buf := make([]byte, 1024)
 
 	for {
-		n, err := conn.Read(buf)
-		
+		length, err := conn.Read(buf)
+
 		if err != nil {
 			fmt.Println("Error in reading input: ", err.Error())
 		}
-		fmt.Println("Received data", buf[:n])
-		conn.Write([]byte("+PONG\r\n"))
+		fmt.Println("Received data", buf[:length])
+
+		rawInput := string(buf[:length])
+		lines := strings.Split(rawInput, "\r\n")
+		fmt.Println(lines)
+
+		// If the input received is an array
+		if len(lines) > 0 && strings.HasPrefix(lines[0], "*") {
+
+			elements := []string{}
+			for i := 1; i < len(lines); i++ {
+				if strings.HasPrefix(lines[i], "$") {
+					elementLength, err := strconv.Atoi(strings.Trim(lines[i][1:], "\r"))
+
+					if err != nil {
+						fmt.Println("Error parsing element length:", err.Error())
+					}
+
+					if i+1 < len(lines) && len(strings.Trim(lines[i+1], "\r")) == elementLength {
+						elements = append(elements, strings.Trim(lines[i+1], "\r"))
+						i++ // Skip the next line as it is part of the current element
+					}
+				}
+
+			}
+
+			if len(elements) == 1 && elements[0] == "PING" {
+				conn.Write([]byte("+PONG\r\n"))
+			}
+
+			if len(elements) == 2 && strings.ToLower(elements[0]) == "echo" {
+				response := fmt.Sprintf("$%d\r\n%s\r\n", len(elements[1]), elements[1])
+				conn.Write([]byte(response))
+			}
+
+		}	
 	}
 
 	return nil
